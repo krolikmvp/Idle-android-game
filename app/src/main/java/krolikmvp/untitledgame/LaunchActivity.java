8f9d51ed737fc29b320db03eb8c0cd01d0961b98
krolikmvp <dkrolikowski6@gmail.com>
2016-09-03 15:53:54 +0200

    added starting screen and main menu

--- app/src/main/java/krolikmvp/untitledgame/LaunchActivity.java
+++ app/src/main/java/krolikmvp/untitledgame/LaunchActivity.java
@@ -1,7 +1,10 @@
 package krolikmvp.untitledgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        ImageView mImageView;
+        mImageView = (ImageView) findViewById(R.id.logo);
+        mImageView.setImageResource(R.drawable.testlogo);
+        Thread thread = new Thread(){
+
+          public void run(){
+
+              try {
+                  sleep(5000);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+              finally{
+                  Intent intent = new Intent("android.intent.action.MAIN_MENU");
+                  startActivity(intent);
+              }
+
+          }
+
+        };
+        thread.start();
     }
 }
---
+++ app/src/main/java/krolikmvp/untitledgame/MainMenu.java
@@ -0,0 +1,19 @@
+package krolikmvp.untitledgame;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}
